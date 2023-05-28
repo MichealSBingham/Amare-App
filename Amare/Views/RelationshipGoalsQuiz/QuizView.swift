985cccf731826d2200b17b7f0b5683eb60036415
Micheal Bingham <micheal@findamare.com>
2023-05-28 17:02:15 -0400

    can dismiss the relationship goal quiz view now can automatically page between each question now

--- Amare/Views/RelationshipGoalsQuiz/QuizView.swift
+++ Amare/Views/RelationshipGoalsQuiz/QuizView.swift
@@ -7,48 +7,111 @@
 
 
 import SwiftUI
-
+/// Relationship Goals Quiz View.  This is the main view that shows the relationship goals questions 
 struct QuizView: View {
+	
+	
+	
+	@State var startDate = Date.now
+	@State var timeElapsed: Int = 0
+	let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+	
+	@State var currentPage = 0
+	
+	
+	@State var disablePaging = false
+	
+	///use this so that the page never disables again
+	@State var neverDisablePagingAgain = false
+	
     
     var body: some View {
         
-        GeometryReader{ proxy in
-            
-            TabView {
-                
-                Group{
-                    
-                    ForEach(RGQuizQuestion.allCases){ question in
-                        
-                        QuestionView(question: question)
-                    }
-                    
-                    
-                }
-                .rotationEffect(.degrees(-90)) // Rotate content
-                                .frame(
-                                    width: proxy.size.width,
-                                    height: proxy.size.height
-                                )
-                
-                
-            }
-           
-            .frame(
-                            width: proxy.size.height, // Height & width swap
-                            height: proxy.size.width
-                        )
-                        .rotationEffect(.degrees(90), anchor: .topLeading) // Rotate TabView
-                        .offset(x: proxy.size.width) // Offset back into screens bounds
-                        .tabViewStyle(
-                            PageTabViewStyle(indexDisplayMode: .never)
-                        )
-            
-        }
+	
+			GeometryReader{ proxy in
+				
+				TabView(selection: $currentPage){
+					
+					
+					Group{
+						
+						BeginQuizView()
+							.tag(0)
+							.onReceive(timer) { firedDate in
+								// This is executed every second
+								timeElapsed = Int(firedDate.timeIntervalSince(startDate))
+								
+								// After 3 seconds, switch to the next page if the user hasn't already done it
+								if timeElapsed == 3 && currentPage == 0{
+									
+										currentPage+=1
+									
+								}
+								
+							}
+							
+						
+						BeginQuizBodyView(disablePaging: $disablePaging, nextPage: nextPage)
+							.tag(1)
+							.onAppear{
+								if neverDisablePagingAgain == false {
+									disablePaging = true
+								}
+								
+							}
+							.onDisappear{
+								neverDisablePagingAgain = true 
+							}
+							
+						
+						
+						
+						ForEach(Array(RGQuizQuestion.allCases.enumerated()), id: \.1){ index, question in
+							
+							QuestionView(question: question)
+								.tag(index + 2 )
+						}
+						
+						
+					}
+					.rotationEffect(.degrees(-90)) // We rotate the content in the tab view so that it's vertical
+									.frame(
+										width: proxy.size.width,
+										height: proxy.size.height
+									)
+					
+					
+				}
+				.disabled(disablePaging)
+				.animation(.easeInOut)
+				.navigationTitle(Text("Unlock Your Relationship Potential"))
+				.navigationBarTitleDisplayMode(.large)
+				// We need to rotate the tab view itself to enable vertical pagging
+			   
+				.frame(
+								width: proxy.size.height, // Height & width swap
+								height: proxy.size.width
+							)
+							.rotationEffect(.degrees(90), anchor: .topLeading) // Rotate TabView
+							.offset(x: proxy.size.width) // Offset back into screens bounds
+							.tabViewStyle(
+								PageTabViewStyle(indexDisplayMode: .never)
+							)
+				
+			}
+		
+		
+		
+		
         
        
       
     }
+	
+	
+	func nextPage() {
+		  currentPage += 1
+	  }
 }
 
 
