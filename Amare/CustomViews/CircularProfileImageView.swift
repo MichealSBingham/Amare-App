cc95b64104148cf2a7e63f1ab78d6d06eee2a423
Micheal S. Bingham <micheal.s.bingham@icloud.com>
2023-07-20 20:50:45 -0400

    Can search users now

---
+++ Amare/CustomViews/CircularProfileImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularProfileImageView.swift
+//  Amare
+//
+//  Created by Micheal Bingham on 7/17/23.
+//
+
+import SwiftUI
+
+struct CircularProfileImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularProfileImageView()
+}
---
+++ Amare/SearchUsersAndFriends/Models/SearchedUser.swift
@@ -0,0 +1,8 @@
+//
+//  SearchedUser.swift
+//  Amare
+//
+//  Created by Micheal Bingham on 7/20/23.
+//
+
+import Foundation
