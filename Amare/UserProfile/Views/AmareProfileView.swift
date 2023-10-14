46c2a3914464c331e691629385f9d4a4a5e2b891
Micheal Bingham <micheal@findamare.com>
2023-10-14 18:00:12 -0400

    FIxing some things in nearby interaction... token sending/retreiving... doesn't work 100% yet, mostly 80%.. net to refrator now and make it work for an actual user.

---
+++ Amare/UserProfile/Views/AmareProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  AmareProfileView.swift
+//  Amare
+//
+//  Created by Micheal Bingham on 10/14/23.
+//
+
+import SwiftUI
+
+struct AmareProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AmareProfileView()
+}
