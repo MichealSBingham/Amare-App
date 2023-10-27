189068190388c81a9ff88e89627b4e95c500714a
Micheal Bingham <micheal@findamare.com>
2023-10-27 19:12:01 -0400

    AD-11 can send winks and recieve when they come but we need to cancel it

---
+++ Amare/Views/UserProfile/Views/Subviews/CustomSegmentedMenu.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSegmentedMenu.swift
+//  Amare
+//
+//  Created by Micheal Bingham on 10/27/23.
+//
+
+import SwiftUI
+
+struct CustomSegmentedMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSegmentedMenu()
+}
