f917f3949c350401d246db871ba95060ed78f7d4
Micheal S. Bingham <micheal.s.bingham@icloud.com>
2023-11-29 13:49:14 -0500

    Add images to upload

---
+++ Amare/Views/Onboarding/ExtraMediaUploadView.swift
@@ -0,0 +1,18 @@
+//
+//  ExtraMediaUploadView.swift
+//  Amare
+//
+//  Created by Micheal Bingham on 11/29/23.
+//
+
+import SwiftUI
+
+struct ExtraMediaUploadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExtraMediaUploadView()
+}
